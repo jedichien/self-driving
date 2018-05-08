c0e2c06a189662ba0dfc917d110f64c1aa7615b3
jedichien <james.chain1990@gmail.com>
2018-05-08 09:56:05 +0000

    init

---
+++ project_7_kidnapped_vehicle/learn/loc_motion.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include "normpdf.h"
+
+using namespace std;
+
+/**
+ * initialize priors
+ */
+vector<float> init_priors(unsigned int map_size, vector<float> landmark_positions, float control_stdev);
+
+/**
+ * motion model
+ */
+float motion_model(float pseudo_position, float movement, vector<float> priors, unsigned int map_size, int control_stdev);
+
+int main() {
+  float control_stdev = 1.0f;
+  // meters vehicle moves per time step 
+  float movement_per_timestamp = 1.0f;
+  // number of x position on map
+  unsigned int map_size = 25;
+  // initialize landmark 
+  vector<float> landmark_positions {5, 10, 20};
+  // initialize priors
+  vector<float> priors = init_priors(map_size, landmark_positions, control_stdev);
+
+  for (unsigned int i = 0; i < map_size; i++) {
+    float pseudo_position = float(i);
+    // get the motion model probability for each x position
+    float motion_prob = motion_model(pseudo_position, movement_per_timestamp, priors, map_size, control_stdev);
+    cout << "pseudo_position: " << pseudo_position << ", " << "motion_prob: " << motion_prob << endl; 
+  }
+  return 0;
+}
+
+float motion_model(float pseudo_position, float movement, 
+        vector<float> priors, unsigned int map_size, int control_stdev) {
+    float position_prob = 0.0f;
+    for (unsigned int j=0; j < map_size; j++) {
+      float next_pesudo_position = float(j);
+      float distance_ij = pseudo_position - next_pesudo_position;
+      // transition probability
+      // The distibution generated consist of standard variance given by control, and mean value given by mean of movement in unit time.
+      float transition_prob = normpdf(distance_ij, movement, control_stdev); 
+      position_prob += transition_prob * priors[j];
+    }
+    return position_prob;
+}
+
+vector<float> init_priors(unsigned int map_size, vector<float> landmark_positions, float control_stdev) {
+  vector<float> priors(map_size, 0.0);
+  // normal distribution value of 2 times standard variance
+  // plus 1 means due to this distribution's centre is 1, so have to shift 1 unit.
+  float normalization_term = landmark_positions.size() * (control_stdev * 2 + 1);
+  for (unsigned int i = 0; i < landmark_positions.size(); i++) {
+    int landmark_centre = landmark_positions[i];
+    priors[landmark_centre] = 1.0f/normalization_term;
+    priors[landmark_centre-1] = 1.0f/normalization_term;
+    priors[landmark_centre+1] = 1.0f/normalization_term; 
+  }
+  return priors;
+}
+
+
---
+++ project_7_kidnapped_vehicle/learn/loc_observation.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include "normpdf.h"
+
+using namespace std;
+
+vector<float> pseudo_range_estimator(vector<float> landmark_positions, float pseudo_position, float distance_max);
+
+float observation_model(vector<float> landmark_positions, vector<float> observations, 
+        vector<float> pseudo_ranges, float observation_stdev);
+
+int main() {
+  // standard deviation
+  float observation_stdev = 1.0f;
+  unsigned int map_size = 25;
+  float distance_max = map_size;
+  vector<float> landmark_positions {5.f, 10.f, 12.f, 20.f};
+  vector<float> observations {5.5f, 13.f, 15.f};
+
+  for (unsigned int i = 0; i < map_size; ++i) {
+    float pseudo_position = float(i);
+    vector<float> pseudo_ranges = pseudo_range_estimator(landmark_positions, pseudo_position, distance_max);
+    // get observation probability
+    float observation_prob = observation_model(landmark_positions, observations, pseudo_ranges, observation_stdev);
+    cout << "pseudo_position: " << pseudo_position << ", observation_prob: " << observation_prob << endl; 
+  }
+  return 0;
+}
+
+float observation_model(vector<float> landmark_positions, vector<float> observations,
+        vector<float> pseudo_ranges, float observation_stdev) {
+  float distance_prob = 1.0f;
+  for (unsigned int z = 0; z < observations.size(); ++z) {
+    float pseudo_range_min = pseudo_ranges[z];
+    // assume the noise behavior of the individual range value from 0~N is independent
+    distance_prob *= normpdf(observations[z], pseudo_range_min, observation_stdev);
+  }
+  return distance_prob;
+}
+
+vector<float> pseudo_range_estimator(vector<float> landmark_positions, float pseudo_position, float distance_max) {
+  vector<float> pseudo_ranges;
+  for (unsigned int l = 0; l < landmark_positions.size(); ++l) {
+    // estimate pseudo range for each single landmark
+    float range_l = landmark_positions[l] - pseudo_position;
+    if (range_l > 0.0f) {
+      pseudo_ranges.push_back(range_l);
+    }
+    else {
+      pseudo_ranges.push_back(distance_max);
+    }
+  }
+  sort(pseudo_ranges.begin(), pseudo_ranges.end()); 
+  return pseudo_ranges;
+}
+
