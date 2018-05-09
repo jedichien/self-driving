f767ba2dca5aeb20195eba80a020ce768e9ab290
jedichien <james.chain1990@gmail.com>
2018-05-09 06:46:34 +0000

    comment detail description

--- project_7_kidnapped_vehicle/learn/loc_observation.cpp
+++ project_7_kidnapped_vehicle/learn/loc_observation.cpp
@@ -16,6 +16,7 @@ int main() {
   unsigned int map_size = 25;
   float distance_max = map_size;
   vector<float> landmark_positions {5.f, 10.f, 12.f, 20.f};
+  // practicle observations range
   vector<float> observations {5.5f, 13.f, 15.f};
 
   for (unsigned int i = 0; i < map_size; ++i) {
@@ -34,6 +35,7 @@ float observation_model(vector<float> landmark_positions, vector<float> observat
   for (unsigned int z = 0; z < observations.size(); ++z) {
     float pseudo_range_min = pseudo_ranges[z];
     // assume the noise behavior of the individual range value from 0~N is independent
+    // porbability based on practicle range of observation distribution
     distance_prob *= normpdf(observations[z], pseudo_range_min, observation_stdev);
   }
   return distance_prob;
@@ -42,7 +44,7 @@ float observation_model(vector<float> landmark_positions, vector<float> observat
 vector<float> pseudo_range_estimator(vector<float> landmark_positions, float pseudo_position, float distance_max) {
   vector<float> pseudo_ranges;
   for (unsigned int l = 0; l < landmark_positions.size(); ++l) {
-    // estimate pseudo range for each single landmark
+    // `estimate` pseudo range for each single landmark
     float range_l = landmark_positions[l] - pseudo_position;
     if (range_l > 0.0f) {
       pseudo_ranges.push_back(range_l);
