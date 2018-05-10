915dfd7120d0cb23399642124326ccbc930db649
jedichien <james.chain1990@gmail.com>
2018-05-10 07:24:55 +0000

    move to norm.h

--- project_7_kidnapped_vehicle/learn/normpdf.h
+++
@@ -1,9 +0,0 @@
-#ifndef NORMAL_DISTRIBUTION_FOR_LEARNING
-#define NORMAL_DISTRIBUTION_FOR_LEARNING
-#include <cmath>
-
-inline float normpdf(float x, float m, float s) {
-  return (1.0/(s*sqrt(2*M_PI))) * exp(-0.5*pow(x-m, 2)/pow(s, 2));
-}
-
-#endif /* NORMAL_DISTRIBUTION_FOR_LEARNING */
