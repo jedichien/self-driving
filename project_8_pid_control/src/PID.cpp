044e0014c544f25f720acd118c7d46da60dd4b8d
jedichien <james.chain1990@gmail.com>
2018-05-17 07:22:03 +0000

    init

---
+++ project_8_pid_control/src/PID.cpp
@@ -0,0 +1,31 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+  this->p_error = 0.0;
+  this->i_error = 0.0;
+  this->d_error = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+}
+
+double PID::TotalError() {
+  return -(Kp*p_error) - (Ki*i_error) - (Kd*d_error);
+}
+
